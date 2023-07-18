34823191338305b2e89e23514fab137045b8047c
Crend King <975235+CrendKing@users.noreply.github.com>
2023-07-18 11:21:44 -0700

    Do not touch my %PATH%

--- lib/install.ps1
+++ lib/install.ps1
@@ -877,6 +877,7 @@ function find_dir_or_subdir($path, $dir) {
 }
 
 function env_add_path($manifest, $dir, $global, $arch) {
+    return
     $env_add_path = arch_specific 'env_add_path' $manifest $arch
     $dir = $dir.TrimEnd('\')
     if ($env_add_path) {
@@ -889,6 +890,7 @@ function env_add_path($manifest, $dir, $global, $arch) {
 }
 
 function env_rm_path($manifest, $dir, $global, $arch) {
+    return
     $env_add_path = arch_specific 'env_add_path' $manifest $arch
     $dir = $dir.TrimEnd('\')
     if ($env_add_path) {
